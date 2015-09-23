70341fb4e14e53192cd9cbb2c8e9302a8e257b7d
Michael Brandt <mbrandt@colorado.edu>
2015-09-23 12:50:22 -0600

    Util fns to convert bboxes between objs and strs
    Change how the object is built and returned in UtilityFunctions to avoid
    problems with `this`.

--- src/scripts/lib/utility_functions.js
+++ src/scripts/lib/utility_functions.js
@@ -17,7 +17,7 @@ define(function () {
     });
   };
 
-  return {
+  var exports = {
     toInitialCaps : function (string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     },
@@ -54,25 +54,6 @@ define(function () {
       }
     },
 
-    getArrayFromjQueryArrayTextContents : function (jQueryArray) {
-      var results = [],
-      removeTags = this.removeTags,
-      removeWhitespace = this.removeWhitespace;
-
-      if (jQueryArray.text() === '') {
-        return undefined;
-      }
-
-      _.each(jQueryArray, function (element) {
-        var text = $(element).text();
-        if (text && text.length > 0) {
-          results.push(removeWhitespace(removeTags(text)));
-        }
-      });
-
-      return results;
-    },
-
     // http://www.textfixer.com/tutorials/javascript-line-breaks.php
     removeWhitespace : function (str) {
       if (str === undefined || str === '') {
@@ -92,58 +73,9 @@ define(function () {
       return html.replace(/</g, '&lt;');
     },
 
-    normalizeDate : function (dateString) {
-      if (dateString && dateString.length && dateString.length > 0) {
-        dateString = this.removeWhitespace(dateString);
-        // Accept only ISO formatted dates
-        if (moment(dateString).isValid()) {
-          return dateString.substr(0, 10);
-        }
-      }
-      return undefined;
-    },
-
     isFloat: function (number) {
       var matches = /^(\-)?(\d*)(\.\d*)?$/.exec(number);
       return matches ? true : false;
-    },
-
-    osGeoBoxToIdentifier: function (osGeoBox) {
-      var bboxArray, cardinals, coords = {};
-
-      if (osGeoBox === undefined || osGeoBox === '') {
-        return '';
-      }
-
-      bboxArray = osGeoBox.split(',');
-      cardinals = ['west', 'south', 'east', 'north'];
-      _.each(bboxArray, function (coordinate, index) {
-        coords[cardinals[index]] = coordinate;
-      });
-
-      return ['N:' + coords.north, 'S:' + coords.south, 'E:' + coords.east, 'W:' + coords.west].join(',');
-    },
-
-    osGeoBoxFromIdentifier: function (identifier) {
-      var geoBox, bboxArray = [], coords = {},
-      cardinals = ['north', 'south', 'east', 'west'];
-
-      if (identifier === undefined) {
-        return '';
-      }
-
-      bboxArray = identifier.split(/,?\ *[NSEW]:/);
-      bboxArray = this.removeEmptyElementsFromArray(bboxArray);
-      if (bboxArray.length === 1) {
-        return '';
-      }
-
-      _.each(bboxArray, function (coordinate, index) {
-        coords[cardinals[index]] = coordinate;
-      });
-
-      geoBox = [coords.west, coords.south, coords.east, coords.north].join(',');
-      return (geoBox === ',,,' ? '' : geoBox);
     },
 
     removeEmptyElementsFromArray : function (array) {
@@ -163,4 +95,90 @@ define(function () {
       return Math.round(number * coefficient) / coefficient;
     }
   };
+
+  exports.getArrayFromjQueryArrayTextContents = function (jQueryArray) {
+    var results = [],
+        removeTags = exports.removeTags,
+        removeWhitespace = exports.removeWhitespace;
+
+    if (jQueryArray.text() === '') {
+      return undefined;
+    }
+
+    _.each(jQueryArray, function (element) {
+      var text = $(element).text();
+      if (text && text.length > 0) {
+        results.push(removeWhitespace(removeTags(text)));
+      }
+    });
+
+    return results;
+  };
+
+  exports.normalizeDate = function (dateString) {
+    if (dateString && dateString.length && dateString.length > 0) {
+      dateString = exports.removeWhitespace(dateString);
+      // Accept only ISO formatted dates
+      if (moment(dateString).isValid()) {
+        return dateString.substr(0, 10);
+      }
+    }
+    return undefined;
+  };
+
+  exports.osGeoBoxToNsewObj = function (osGeoBox) {
+    var bboxArray, cardinals, coords = {};
+
+    if (osGeoBox === undefined || osGeoBox === '') {
+      return '';
+    }
+
+    bboxArray = osGeoBox.split(',');
+    cardinals = ['west', 'south', 'east', 'north'];
+    _.each(bboxArray, function (coordinate, index) {
+      coords[cardinals[index]] = coordinate;
+    });
+
+    return coords;
+  };
+
+  exports.nsewObjToIdentifier = function (coords) {
+    if (coords.north && coords.south && coords.east && coords.west) {
+      return ['N:' + coords.north, 'S:' + coords.south, 'E:' + coords.east, 'W:' + coords.west].join(',');
+    } else {
+      return '';
+    }
+  };
+
+  exports.osGeoBoxToIdentifier = function (osGeoBox) {
+    return exports.nsewObjToIdentifier(exports.osGeoBoxToNsewObj(osGeoBox));
+  };
+
+  exports.osGeoBoxFromIdentifier = function (identifier) {
+    var geoBox, bboxArray = [], coords = {},
+        cardinals = ['north', 'south', 'east', 'west'];
+
+    if (identifier === undefined) {
+      return '';
+    }
+
+    bboxArray = identifier.split(/,?\ *[NSEW]:/);
+    bboxArray = exports.removeEmptyElementsFromArray(bboxArray);
+    if (bboxArray.length === 1) {
+      return '';
+    }
+
+    _.each(bboxArray, function (coordinate, index) {
+      coords[cardinals[index]] = coordinate;
+    });
+
+    geoBox = [coords.west, coords.south, coords.east, coords.north].join(',');
+    return (geoBox === ',,,' ? '' : geoBox);
+  };
+
+  exports.nsewObjFromIdentifier = function (identifier) {
+    return exports.osGeoBoxToNsewObj(exports.osGeoBoxFromIdentifier(identifier));
+  };
+
+  return exports;
 });
