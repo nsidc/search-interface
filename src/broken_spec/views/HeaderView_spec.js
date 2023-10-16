58a7ace0ebb60901476775ebca5510307d2be86e
Scott Lewis <scott.lewis@colorado.edu>
2023-10-16 16:42:44 -0600

    Adding some notes to a test that may take more work to fix

--- src/broken_spec/views/HeaderView_spec.js
+++ src/broken_spec/views/HeaderView_spec.js
@@ -1,35 +1,25 @@
-// import MainHeaderView from '../../views/MainHeaderView';
+// TODO: Jest is having a hard time with one of the openlayer files that's a descendent dependency.
+// Giving an error about an import statement outside a module.  That may need to be figured out to
+// get these tests working properly.
+
+
+import HeaderView from '../../views/HeaderView.js';
 
 // var createFakeView = function() { return sinon.createStubInstance(Backbone.View); };
 // var SearchCriteriaView = sinon.stub().returns(createFakeView());
 
-describe.skip('Main Header View', function () {
+describe('Header View', function () {
 
     describe('rendering the view', function () {
-        var mainHeader;
+        let mainHeader;
 
         beforeEach(function () {
             SearchCriteriaView.resetHistory();
         });
 
-        describe('rendering the ADE view', function () {
-            beforeEach(function () {
-                mainHeader = new MainHeaderView({templateId: 'ADE'}).render();
-            });
-
-            it('should create a correctly structured element as provided', function () {
-                expect(mainHeader.$el.is('div')).toBeTruthy();
-                expect(mainHeader.$el.find('#head-title').length).toBe(1);
-            });
-
-            it('should create a Search Criteria subview', function () {
-                expect(SearchCriteriaView.mock.calls.length).toBe(1);
-            });
-        });
-
         describe('rendering the NSIDC view', function () {
             beforeEach(function () {
-                mainHeader = new MainHeaderView({templateId: 'NSIDC'}).render();
+                mainHeader = new HeaderView({templateId: 'NSIDC'}).render();
             });
 
             it('should create a correctly structured element as provided', function () {
@@ -43,7 +33,7 @@ describe.skip('Main Header View', function () {
         });
 
         it('throws if the template ID is not supported', function () {
-            mainHeader = new MainHeaderView({templateId: 'no_such_template'});
+            mainHeader = new HeaderView({templateId: 'no_such_template'});
             expect(function () { mainHeader.render(); }).toThrow();
         });
     });
