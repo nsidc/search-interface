18786350396657cbdbcc4f199d8908b9fc544c24
Scott Lewis <scott.lewis@colorado.edu>
2023-10-17 15:28:28 -0600

    Updating release.sh to be more POSIX-compliant

--- release.sh
+++ release.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
-#
-VERSION=`node -p "require('./package.json').version"`
-DATE_STR="$(date +%F)"
-CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-
-if [[ ! ${npm_config_git_tag_version+tag} ]]; then
-   echo "Tagging $CURRENT_BRANCH with version $VERSION ($DATE_STR)\n"
-fi
 
 # Update CHANGELOG.md with current version and date if this is not a release
 # candidate version bump.
-if [[ ! $VERSION =~ - ]]; then
+if [ -n $VERSION ]; then
   echo "Updating CHANGELOG with version number and date: $VERSION ($DATE_STR)"
   sed -i '' "s/^## Unreleased$/## v$VERSION ($DATE_STR)/" CHANGELOG.md
   git add CHANGELOG.md
